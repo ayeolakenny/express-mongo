2028a9062a3f1defe84fe2aa1ecedcd56a165776
ayeolakenny <ayeolakenny@gmail.com>
2022-10-10 22:02:40 +0100

    init

---
+++ app.js
@@ -0,0 +1,143 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+const methodOverride = require("method-override");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const app = express();
+
+dotenv.config();
+
+const DATABASE_URL = process.env.DATABASE_URL;
+
+const CONFIG = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+mongoose.connect(DATABASE_URL, CONFIG);
+mongoose.connection
+  .on("open", () => console.log("Connected to Mongoose"))
+  .on("close", () => console.log("Disconnected from Mongoose"))
+  .on("error", (error) => console.log(error));
+
+const { Schema, model } = mongoose;
+
+const userSchema = new Schema({
+  username: String,
+  password: String,
+  email: String,
+  amount: Number,
+});
+
+const User = model("User", userSchema);
+
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(morgan("tiny"));
+app.use(methodOverride("_method"));
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => res.type("html").send(html));
+
+app.post("/create", (req, res) => {
+  const user = new User({
+    ...req.body,
+  });
+  user
+    .save()
+    .then((data) => {
+      res.send(data);
+      console.log("Saved");
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while creating the Message.",
+      });
+    });
+});
+
+app.post("/user/login", (req, res) => {
+  User.findOne(req.body)
+    .then((data) => {
+      console.log(data);
+      if (!data) {
+        return res.status(404).send({
+          message: "User not found",
+        });
+      }
+      res.send(data);
+    })
+    .catch((err) => {
+      if (err.kind === "ObjectId") {
+        return res.status(404).send({
+          message: "User not found",
+        });
+      }
+      return res.status(500).send({
+        message: "Error retrieving message with id " + req.params.messageId,
+      });
+    });
+});
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => console.log(`Now Listening on port ${PORT}`));
+
+const html = `
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Hello from Render!</title>
+    <script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js"></script>
+    <script>
+      setTimeout(() => {
+        confetti({
+          particleCount: 100,
+          spread: 70,
+          origin: { y: 0.6 },
+          disableForReducedMotion: true
+        });
+      }, 500);
+    </script>
+    <style>
+      @import url("https://p.typekit.net/p.css?s=1&k=vnd5zic&ht=tk&f=39475.39476.39477.39478.39479.39480.39481.39482&a=18673890&app=typekit&e=css");
+      @font-face {
+        font-family: "neo-sans";
+        src: url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff2"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/d?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/a?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("opentype");
+        font-style: normal;
+        font-weight: 700;
+      }
+      html {
+        font-family: neo-sans;
+        font-weight: 700;
+        font-size: calc(62rem / 16);
+      }
+      body {
+        background: white;
+      }
+      section {
+        border-radius: 1em;
+        padding: 1em;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        margin-right: -50%;
+        transform: translate(-50%, -50%);
+      }
+    </style>
+  </head>
+  <body>
+    <section>
+      Hello from express!
+    </section>
+  </body>
+</html>
+`;
